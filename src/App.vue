c4472b06a3616fa1e4d24e387b9fbfe12f25102b
Chris <chris.brix0629@gmail.com>
2023-02-06 13:13:58 +0100

    Error component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 import Home from "./components/Home.vue";
 import Detail from "./components/Detail.vue";
 import Summary from "./components/Summary.vue";
+import Error from "./components/Error.vue";
 </script>
 
 <template>
   <div
     class="bg-white flex justify-center items-center absolute top-0 right-0 bottom-0 left-0"
   >
-    <Detail />
+    <Error />
   </div>
 </template>
---
+++ src/components/Error.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container text-center w-96 py-12 px-8">
+    <h1 class="text-4xl font-bold">Ooops</h1>
+    <p class="mt-8">
+      Your age is over our accepted limit. We are sorry but we cannot insure you
+      now
+    </p>
+    <button class="btn-primary mt-8">OK :(</button>
+  </div>
+</template>
