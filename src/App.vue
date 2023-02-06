94cf0de084b7bccde0ff4f1fbbd6133705967841
Chris <chris.brix0629@gmail.com>
2023-02-06 12:45:41 +0100

    detail component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Home from "./components/Home.vue";
+import Detail from "./components/Detail.vue";
 </script>
 
 <template>
   <div
     class="bg-white flex justify-center items-center absolute top-0 right-0 bottom-0 left-0"
   >
-    <Home />
+    <Detail />
   </div>
 </template>
